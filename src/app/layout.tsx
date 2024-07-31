db968215f78d3f2dec13842b497aac194623e782
nikenhpsr <niken18003@mail.unpad.ac.id>
2024-07-31 21:16:29 +0700

    feat: add google tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import GoogleTagManager from "@/components/SEO/GoogleTagManager";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,8 +19,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="scroll-smooth">
-      <head></head>{" "}
+      <head>
+        <GoogleTagManager />
+      </head>{" "}
       <body className={inter.className}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5T2TP73B"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <Navbar />
         {children}
       </body>
